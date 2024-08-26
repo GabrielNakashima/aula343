ce774f132e6a5f05d266aabd95d68ecb0e07872e
Naka <gabrielluni@gmail.com>
2024-08-26 09:05:15 -0300

    Criando componente

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter,RouterProvider } from 'react-router-dom'
+import Error from './routes/Error.jsx'
+import Aluno from './routes/Aluno.jsx'
+import Home from './routes/Home.jsx'
 
+{/*Criando a função createbrowserrouter*/}
+
+const router=createBrowserRouter([
+  {
+    //Chamado os elementos pa
+    path:'/',element:<App/>,
+    errorElement:<Error/>,
+
+    /*Chamando os elementos filhos*/
+    children:[
+      /*Criado a rota home*/
+      {path:'/',element:<Home/>},
+      //Criado a rota aluno
+      {path:'/aluno',element:<Aluno/>}
+    ]
+
+  }
+])
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    {/*Criando o props que será renderzado*/}
+    <RouterProvider router={router}/>
   </StrictMode>,
 )
---
+++ src/routes/Error.jsx
@@ -0,0 +1,8 @@
+const Error=()=>{
+    return(
+        <>
+        <h1>404- Página encontrada (tomou parry diff)</h1>
+        </>
+    )
+}
+export default Error
